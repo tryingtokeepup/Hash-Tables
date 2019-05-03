10829bcf90fa133cf105b278d0885e2585e24279
tryingtokeepup <kl.hawaii@gmail.com>
2019-05-02 21:37:25 -0700

    i have no idea how the remove is working, but ill go over it again tomorrow. for now, i'm exhausted. MVP done. going to get some rest.

--- full_hashtable/hashtables.c
+++ full_hashtable/hashtables.c
@@ -83,19 +83,11 @@ HashTable *create_hash_table(int capacity)
   return ht;
 }
 
-/*
-  Fill this in.
-
-  Inserting values to the same index with different keys should be
-  added to the corresponding LinkedPair list.
-
-  Inserting values to the same index with existing keys can overwrite
-  the value in th existing LinkedPair list.
- */
 void hash_table_insert(HashTable *ht, char *key, char *value)
 {
   //1) Take key, hash this number to get a num less than capacity.
   unsigned int hashed_index = hash(key, ht->capacity);
+
   //2) Take this num, use this as the index number/bucket number that we will store the pair.
   if (ht->storage[hashed_index] == NULL)
   {
@@ -144,26 +136,94 @@ void hash_table_insert(HashTable *ht, char *key, char *value)
 
   Don't forget to free any malloc'ed memory!
  */
+// void hash_table_remove(HashTable *ht, char *key)
+// {
+//   // 1) We need to remove the key value pair at the hashed_index, so first we need to get the hash
+//   unsigned int hashed_index = hash(key, ht->capacity);
+//   // 2) now, once we have the index, go through the linked listand find the node with the same key. once we do, remove it.
+//   LinkedPair *current_node = ht->storage[hashed_index]; // the index number always points to the first node
+//   LinkedPair *previous_node = NULL;
+//   while (current_node != NULL && (*current_node).key != key)
+//   {
+
+//     previous_node = current_node;
+//     current_node = (*current_node).next;
+//   }
+//   // if we exited the above while loop and the current_node is  NULL, it must mean we didn't find the key
+//   if (current_node == NULL)
+//   {
+//     printf("Failed to find the key, check again please. %s\n", key);
+//   }
+//   else
+//   {
+//     // if this is the first node in the list, we should check for that by checking its previous_node value.
+//     if (previous_node == NULL)
+//     {
+//       // take whatever is the next node after current_node, and put that value into the space that is going to
+//       // be vacated by the current_node after it is removed
+//       ht->storage[hashed_index] = (*current_node).next;
+//     }
+//     else
+//     {
+//       // tricky bugger, but you need to connect the empty gap between the prev node and the next node by
+//       // bridging the two this way
+//       (*previous_node).next = (*current_node).next;
+//     }
+//     printf("successfully destroyed pair\n");
+//     destroy_pair(current_node);
+//     (*current_node).key = NULL;
+//     (*current_node).value = NULL;
+//   }
+// }
 void hash_table_remove(HashTable *ht, char *key)
 {
-  // 1) We need to remove the key value pair at the hashed_index, so first we need to get the hash
-  unsigned int hashed_index = hash(key, ht->capacity);
-  // 2) now, once we have the index, go through the linked listand find the node with the same key. once we do, remove it.
-  LinkedPair *head_node = ht->storage[hashed_index]; // the index number always points to the first node
-  LinkedPair *next_node = (*head_node).next;
-  while (next_node != NULL && strcmp((*head_node).key, key) != 0)
-  {
+  // hash the key to get an array index
+  unsigned int index = hash(key, ht->capacity);
 
-    head_node = next_node;
-    next_node = (*head_node).next;
-  }
-  if (strcmp(head_node->key, key) == 0)
+  // If element of array index is NULL, return null
+  //    exit function
+  if (ht->storage[index] == NULL)
   {
-    destroy_pair(head_node);
-    head_node = NULL;
+    printf("Key \"%s\" was not found in the hash table\n", key);
+    return;
+  }
+
+  // If the head node in the link list is a match...
+  //    make the array index equal next node as the
+  //    new head (or NULL)
+  //    exit function
+  LinkedPair *curr_node = ht->storage[index];
+  if (strcmp(curr_node->key, ht->storage[index]->key) == 0)
+  {
+    ht->storage[index] = curr_node->next;
+    destroy_pair(curr_node);
+    return;
+  }
+
+  // Search through the link list for the node key matching given key...
+  LinkedPair *prev_node = curr_node;
+  curr_node = prev_node->next;
+
+  while (curr_node != NULL && strcmp(curr_node->key, key) != 0)
+  {
+    prev_node = curr_node;
+    curr_node = prev_node->next;
+  }
+
+  // If key was found in the current node of the link list
+  if (strcmp(curr_node->key, key) == 0)
+  {
+    // remove the current node from the link list
+    prev_node->next = curr_node->next;
+    // free the memory of the current node
+    destroy_pair(curr_node);
+    curr_node = NULL;
+  }
+  else
+  {
+    printf("Key \"%s\" was not found in the Link List\n", key);
   }
 }
-
 /*
   Fill this in.
 
@@ -176,25 +236,18 @@ char *hash_table_retrieve(HashTable *ht, char *key)
 {
   // take the key, hash it, then take that array, traverse the linked_list for that key
   unsigned int hashed_index = hash(key, ht->capacity);
-  LinkedPair *head_node = ht->storage[hashed_index]; // the index number always points to the first node
-  LinkedPair *next_node = (*head_node).next;
+  LinkedPair *current_node = ht->storage[hashed_index]; // the index number always points to the first node
 
-  while (next_node != NULL && strcmp((*head_node).key, key) != 0) // if we find the key OR the next_node is NULL, we exit
+  while (current_node != NULL) // if we find the key OR the next_node is NULL, we exit
   {
 
-    head_node = next_node;
-    next_node = (*head_node).next;
+    if (strcmp(current_node->key, key) == 0)
+    {
+      return current_node->value;
+    }
+    current_node = (*current_node).next;
   }
-  // should have head
-  if (strcmp(head_node->key, key) == 0)
-  {
-    return head_node;
-  }
-  else
-  {
-
-    return NULL;
-  }
+  return NULL;
 }
 
 /*
@@ -204,6 +257,25 @@ char *hash_table_retrieve(HashTable *ht, char *key)
  */
 void destroy_hash_table(HashTable *ht)
 {
+  for (int i = 0; i < ht->capacity; i++)
+  {
+    if (ht->storage[i] != NULL)
+    {
+      LinkedPair *current_node = ht->storage[i]; // the index number always points to the first node
+      LinkedPair *next_node = (*current_node).next;
+      // loop through linked_list, destroying and freeing each pair
+      while (next_node != NULL)
+      {
+        destroy_pair(current_node);
+        current_node = next_node;
+        next_node = (*current_node).next;
+      }
+      // free that last pair
+      destroy_pair(current_node);
+    }
+  }
+  free(ht->storage);
+  free(ht);
 }
 
 /*
@@ -216,9 +288,21 @@ void destroy_hash_table(HashTable *ht)
  */
 HashTable *hash_table_resize(HashTable *ht)
 {
-  HashTable *new_ht;
+  HashTable *resized_ht = create_hash_table(2 * ht->capacity);
+  LinkedPair *current_pair;
 
-  return new_ht;
+  for (int i = 0; i < ht->capacity; i++)
+  {
+
+    current_pair = ht->storage[i];
+    while (current_pair != NULL)
+    {
+      hash_table_insert(resized_ht, (*current_pair).key, (*current_pair).value);
+      current_pair = (*current_pair).next;
+    }
+  }
+
+  return resized_ht;
 }
 
 #ifndef TESTING
@@ -241,7 +325,7 @@ int main(void)
   printf("\nResizing hash table from %d to %d.\n", old_capacity, new_capacity);
 
   destroy_hash_table(ht);
-
+  printf("sucessfully destroyed the table.\n");
   return 0;
 }
 #endif
